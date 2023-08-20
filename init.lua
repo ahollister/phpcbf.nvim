f8bb3ac465673ca0cb96d08cd7ca8a62f3c7e64f
ahollister <aaahollister@gmail.com>
2023-08-20 20:54:56 +0100

    Initial

---
+++ init.lua
@@ -0,0 +1,13 @@
+local config = require("config")
+local phpcbf = {}
+local user_opts = {}
+
+phpcbf.setup = function(opts)
+	opts = opts or {}
+	user_opts.auto_format = opts.auto_format or false
+	user_opts.phpcbf_path = opts.phpcbf_path or nil
+	user_opts.phpcbf_ruleset = opts.phpcbf_ruleset or nil
+	config.set_user_opts(user_opts)
+end
+
+return phpcbf
