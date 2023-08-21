86dfad04a05151517191da8dd9c95741ff55c919
ahollister <aaahollister@gmail.com>
2023-08-21 10:22:27 +0100

    Better comments and config syntax

--- lua/phpcbf/init.lua
+++ lua/phpcbf/init.lua
@@ -1,14 +1,16 @@
 local config = require("phpcbf.config")
 local command = require("phpcbf.command")
+
 local phpcbf = {}
-local user_opts = {}
 
 phpcbf.setup = function(opts)
 	opts = opts or {}
-	user_opts.auto_format = opts.auto_format or false
-	user_opts.phpcbf_path = opts.phpcbf_path or nil
-	user_opts.phpcbf_ruleset = opts.phpcbf_ruleset or nil
-	config.set_user_opts(user_opts)
+
+	config.set_user_opts({
+		auto_format = opts.auto_format or false,
+		phpcbf_path = opts.phpcbf_path or nil,
+		phpcbf_ruleset = opts.phpcbf_ruleset or nil,
+	})
 
 	-- Create the PHPCBF command
 	command.create_command()
--- lua/phpcbf/utils.lua
+++ lua/phpcbf/utils.lua
@@ -2,7 +2,7 @@ local config = require("phpcbf.config")
 
 local utils = {}
 
--- Get the current setting for phpcbf path
+-- Get the setting for phpcbf path
 function utils.get_phpcbf_path()
 	if config.user_opts.phpcbf_path then
 		return config.user_opts.phpcbf_path
@@ -10,7 +10,7 @@ function utils.get_phpcbf_path()
 	print("no phpcbf path set")
 end
 
--- Get the path to the phpcbf executable
+-- Get the setting for phpcbf ruleset
 function utils.get_phpcbf_ruleset()
 	if config.user_opts.phpcbf_ruleset then
 		return config.user_opts.phpcbf_ruleset
@@ -18,7 +18,7 @@ function utils.get_phpcbf_ruleset()
 	print("no phpcbf ruleset")
 end
 
--- Get the path to the phpcbf executable
+-- Get the setting for auto_format
 function utils.get_auto_format()
 	if config.user_opts.auto_format then
 		return config.user_opts.auto_format
